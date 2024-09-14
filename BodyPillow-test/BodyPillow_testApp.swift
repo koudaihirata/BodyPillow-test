0d7fc98f9ec05b402df323f350c1dea5cb273c15
koudaihirata <2230051@ecc.ac.jp>
2024-09-15 00:17:16 +0900

    Initial Commit

---
+++ BodyPillow-test/BodyPillow_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPillow_testApp.swift
+//  BodyPillow-test
+//
+//  Created by Kodai Hirata on 2024/09/15.
+//
+
+import SwiftUI
+
+@main
+struct BodyPillow_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
